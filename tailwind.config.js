57cf337cfc3b8c56bd5fc64360ba270fa4b67654
Alex Orpi <orpi.alex@hotmail.com>
2023-10-31 06:03:56 +0100

    added some extended properties for color and font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './public/index.html'],
+  content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      },
+    },
   },
+
   plugins: [],
 };
